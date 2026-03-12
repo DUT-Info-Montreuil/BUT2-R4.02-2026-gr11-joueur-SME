45c5d1662b0a354f991fe94b25dfa02d9c6057df
mbouklikha <m.bouklikha@yahoo.com>
2026-03-12 11:07:17 +0100

    detail de langueEnum

--- src/main/java/universite_Paris8/iut/qdev/tp2026/gr11/enums/LangueEnum.java
+++ src/main/java/universite_Paris8/iut/qdev/tp2026/gr11/enums/LangueEnum.java
@@ -1,8 +1,24 @@
 package universite_Paris8.iut.qdev.tp2026.gr11.enums;
 
 public enum LangueEnum {
-    FR,
-    EN,
-    ES,
-    DE
+    FR(1, "Français"),
+    EN(2, "Anglais"),
+    ES(3, "Espagnol"),
+    DE(4, "Allemand");
+
+    private final int id;
+    private final String nomComplet;
+
+    LangueEnum(int id, String nomComplet) {
+        this.id = id;
+        this.nomComplet = nomComplet;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNomComplet() {
+        return nomComplet;
+    }
 }
